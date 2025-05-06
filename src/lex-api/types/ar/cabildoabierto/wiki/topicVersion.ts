5a7e6bff21101d2e0d9e25fef2926341f16ee892
Luca Zanela <lucazanela@hotmail.com>
2025-05-05 21:37:38 -0300

    visualizaciones

---
+++ src/lex-api/types/ar/cabildoabierto/wiki/topicVersion.ts
@@ -0,0 +1,219 @@
+/**
+ * GENERATED CODE - DO NOT MODIFY
+ */
+import { type ValidationResult, BlobRef } from '@atproto/lexicon'
+import { CID } from 'multiformats/cid'
+import { validate as _validate } from '../../../../lexicons'
+import {
+  type $Typed,
+  is$typed as _is$typed,
+  type OmitKey,
+} from '../../../../util'
+import type * as ArCabildoabiertoActorDefs from '../actor/defs.js'
+import type * as AppBskyActorDefs from '../../../app/bsky/actor/defs.js'
+
+const is$typed = _is$typed,
+  validate = _validate
+const id = 'ar.cabildoabierto.wiki.topicVersion'
+
+export interface Record {
+  $type: 'ar.cabildoabierto.wiki.topicVersion'
+  id: string
+  text: BlobRef
+  format?: string
+  props?: TopicProp[]
+  message?: string
+  createdAt: string
+  [k: string]: unknown
+}
+
+const hashRecord = 'main'
+
+export function isRecord<V>(v: V) {
+  return is$typed(v, id, hashRecord)
+}
+
+export function validateRecord<V>(v: V) {
+  return validate<Record & V>(v, id, hashRecord, true)
+}
+
+export interface TopicView {
+  $type?: 'ar.cabildoabierto.wiki.topicVersion#topicView'
+  id: string
+  uri: string
+  cid: string
+  currentVersion?: string
+  record?: { [_ in string]: unknown }
+  text: string
+  format?: string
+  props?: TopicProp[]
+  lastEdit: string
+  createdAt: string
+  author: ArCabildoabiertoActorDefs.ProfileViewBasic
+}
+
+const hashTopicView = 'topicView'
+
+export function isTopicView<V>(v: V) {
+  return is$typed(v, id, hashTopicView)
+}
+
+export function validateTopicView<V>(v: V) {
+  return validate<TopicView & V>(v, id, hashTopicView)
+}
+
+export interface TopicHistory {
+  $type?: 'ar.cabildoabierto.wiki.topicVersion#topicHistory'
+  id: string
+  versions: VersionInHistory[]
+}
+
+const hashTopicHistory = 'topicHistory'
+
+export function isTopicHistory<V>(v: V) {
+  return is$typed(v, id, hashTopicHistory)
+}
+
+export function validateTopicHistory<V>(v: V) {
+  return validate<TopicHistory & V>(v, id, hashTopicHistory)
+}
+
+export interface VersionInHistory {
+  $type?: 'ar.cabildoabierto.wiki.topicVersion#versionInHistory'
+  uri: string
+  cid: string
+  createdAt: string
+  author: AppBskyActorDefs.ProfileViewBasic
+  message?: string
+  viewer?: TopicVersionViewerState
+  status?: TopicVersionStatus
+  addedChars?: number
+  removedChars?: number
+  props?: TopicProp[]
+}
+
+const hashVersionInHistory = 'versionInHistory'
+
+export function isVersionInHistory<V>(v: V) {
+  return is$typed(v, id, hashVersionInHistory)
+}
+
+export function validateVersionInHistory<V>(v: V) {
+  return validate<VersionInHistory & V>(v, id, hashVersionInHistory)
+}
+
+export interface TopicVersionViewerState {
+  $type?: 'ar.cabildoabierto.wiki.topicVersion#topicVersionViewerState'
+  accept?: string
+  reject?: string
+}
+
+const hashTopicVersionViewerState = 'topicVersionViewerState'
+
+export function isTopicVersionViewerState<V>(v: V) {
+  return is$typed(v, id, hashTopicVersionViewerState)
+}
+
+export function validateTopicVersionViewerState<V>(v: V) {
+  return validate<TopicVersionViewerState & V>(
+    v,
+    id,
+    hashTopicVersionViewerState,
+  )
+}
+
+export interface TopicVersionStatus {
+  $type?: 'ar.cabildoabierto.wiki.topicVersion#topicVersionStatus'
+  voteCounts: CategoryVotes[]
+}
+
+const hashTopicVersionStatus = 'topicVersionStatus'
+
+export function isTopicVersionStatus<V>(v: V) {
+  return is$typed(v, id, hashTopicVersionStatus)
+}
+
+export function validateTopicVersionStatus<V>(v: V) {
+  return validate<TopicVersionStatus & V>(v, id, hashTopicVersionStatus)
+}
+
+export interface CategoryVotes {
+  $type?: 'ar.cabildoabierto.wiki.topicVersion#categoryVotes'
+  accepts: number
+  rejects: number
+  category: string
+}
+
+const hashCategoryVotes = 'categoryVotes'
+
+export function isCategoryVotes<V>(v: V) {
+  return is$typed(v, id, hashCategoryVotes)
+}
+
+export function validateCategoryVotes<V>(v: V) {
+  return validate<CategoryVotes & V>(v, id, hashCategoryVotes)
+}
+
+export interface TopicProp {
+  $type?: 'ar.cabildoabierto.wiki.topicVersion#topicProp'
+  name: string
+  value: $Typed<StringProp> | $Typed<StringListProp> | { $type: string }
+}
+
+const hashTopicProp = 'topicProp'
+
+export function isTopicProp<V>(v: V) {
+  return is$typed(v, id, hashTopicProp)
+}
+
+export function validateTopicProp<V>(v: V) {
+  return validate<TopicProp & V>(v, id, hashTopicProp)
+}
+
+export interface StringProp {
+  $type?: 'ar.cabildoabierto.wiki.topicVersion#stringProp'
+  value: string
+}
+
+const hashStringProp = 'stringProp'
+
+export function isStringProp<V>(v: V) {
+  return is$typed(v, id, hashStringProp)
+}
+
+export function validateStringProp<V>(v: V) {
+  return validate<StringProp & V>(v, id, hashStringProp)
+}
+
+export interface StringListProp {
+  $type?: 'ar.cabildoabierto.wiki.topicVersion#stringListProp'
+  value: string[]
+}
+
+const hashStringListProp = 'stringListProp'
+
+export function isStringListProp<V>(v: V) {
+  return is$typed(v, id, hashStringListProp)
+}
+
+export function validateStringListProp<V>(v: V) {
+  return validate<StringListProp & V>(v, id, hashStringListProp)
+}
+
+export interface TopicViewBasic {
+  $type?: 'ar.cabildoabierto.wiki.topicVersion#topicViewBasic'
+  id: string
+  props?: TopicProp[]
+  popularity?: number[]
+  lastEdit?: string
+}
+
+const hashTopicViewBasic = 'topicViewBasic'
+
+export function isTopicViewBasic<V>(v: V) {
+  return is$typed(v, id, hashTopicViewBasic)
+}
+
+export function validateTopicViewBasic<V>(v: V) {
+  return validate<TopicViewBasic & V>(v, id, hashTopicViewBasic)
+}
---
+++ src/lex-server/types/ar/cabildoabierto/embed/visualization.ts
@@ -0,0 +1,131 @@
+/**
+ * GENERATED CODE - DO NOT MODIFY
+ */
+import { type ValidationResult, BlobRef } from '@atproto/lexicon'
+import { CID } from 'multiformats/cid'
+import { validate as _validate } from '../../../../lexicons'
+import {
+  type $Typed,
+  is$typed as _is$typed,
+  type OmitKey,
+} from '../../../../util'
+
+const is$typed = _is$typed,
+  validate = _validate
+const id = 'ar.cabildoabierto.embed.visualization'
+
+export interface Main {
+  $type?: 'ar.cabildoabierto.embed.visualization'
+  spec:
+    | $Typed<DatasetVisualization>
+    | $Typed<HemicycleVisualization>
+    | $Typed<TopicListVisualization>
+    | { $type: string }
+}
+
+const hashMain = 'main'
+
+export function isMain<V>(v: V) {
+  return is$typed(v, id, hashMain)
+}
+
+export function validateMain<V>(v: V) {
+  return validate<Main & V>(v, id, hashMain)
+}
+
+export interface DatasetVisualization {
+  $type?: 'ar.cabildoabierto.embed.visualization#datasetVisualization'
+  dataset?: string
+  spec?:
+    | $Typed<Barplot>
+    | $Typed<Scatterplot>
+    | $Typed<Histogram>
+    | { $type: string }
+  title?: string
+}
+
+const hashDatasetVisualization = 'datasetVisualization'
+
+export function isDatasetVisualization<V>(v: V) {
+  return is$typed(v, id, hashDatasetVisualization)
+}
+
+export function validateDatasetVisualization<V>(v: V) {
+  return validate<DatasetVisualization & V>(v, id, hashDatasetVisualization)
+}
+
+export interface HemicycleVisualization {
+  $type?: 'ar.cabildoabierto.embed.visualization#hemicycleVisualization'
+}
+
+const hashHemicycleVisualization = 'hemicycleVisualization'
+
+export function isHemicycleVisualization<V>(v: V) {
+  return is$typed(v, id, hashHemicycleVisualization)
+}
+
+export function validateHemicycleVisualization<V>(v: V) {
+  return validate<HemicycleVisualization & V>(v, id, hashHemicycleVisualization)
+}
+
+export interface TopicListVisualization {
+  $type?: 'ar.cabildoabierto.embed.visualization#topicListVisualization'
+}
+
+const hashTopicListVisualization = 'topicListVisualization'
+
+export function isTopicListVisualization<V>(v: V) {
+  return is$typed(v, id, hashTopicListVisualization)
+}
+
+export function validateTopicListVisualization<V>(v: V) {
+  return validate<TopicListVisualization & V>(v, id, hashTopicListVisualization)
+}
+
+export interface Barplot {
+  $type?: 'ar.cabildoabierto.embed.visualization#barplot'
+  xlabel?: string
+  ylabel?: string
+}
+
+const hashBarplot = 'barplot'
+
+export function isBarplot<V>(v: V) {
+  return is$typed(v, id, hashBarplot)
+}
+
+export function validateBarplot<V>(v: V) {
+  return validate<Barplot & V>(v, id, hashBarplot)
+}
+
+export interface Scatterplot {
+  $type?: 'ar.cabildoabierto.embed.visualization#scatterplot'
+  xlabel?: string
+  ylabel?: string
+}
+
+const hashScatterplot = 'scatterplot'
+
+export function isScatterplot<V>(v: V) {
+  return is$typed(v, id, hashScatterplot)
+}
+
+export function validateScatterplot<V>(v: V) {
+  return validate<Scatterplot & V>(v, id, hashScatterplot)
+}
+
+export interface Histogram {
+  $type?: 'ar.cabildoabierto.embed.visualization#histogram'
+  xlabel?: string
+  normalized?: boolean
+}
+
+const hashHistogram = 'histogram'
+
+export function isHistogram<V>(v: V) {
+  return is$typed(v, id, hashHistogram)
+}
+
+export function validateHistogram<V>(v: V) {
+  return validate<Histogram & V>(v, id, hashHistogram)
+}
